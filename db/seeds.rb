2d742d977ded962122c01d7039d3df0b46bea7c4
SunnySparks <Josediazt6@outlook.com>
2022-01-22 16:10:05 -0600

    Create db seeds and fix rubocop

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Message.create(      {
+  greeting: 'Cheers, mate'
+},
+{
+  greeting: 'Hi, friend'
+},
+{
+  greeting: 'G\'day'
+},
+{
+  greeting: 'Hello, Peter'
+},
+{
+  greeting: 'Buenas, buenaaaaaaaaaaaaaaaaaas'
+})
