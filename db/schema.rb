93612a1ae8633d1dfd98a66a62f6ee27912d5eef
SunnySparks <Josediazt6@outlook.com>
2022-01-22 15:58:17 -0600

    Create database and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_21_164129) do
+ActiveRecord::Schema.define(version: 2022_01_22_203407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.string "string"
+    t.string "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "reservations", force: :cascade do |t|
     t.bigint "user_id", null: false
